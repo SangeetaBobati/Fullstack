4f7631947ab18ea50574875f4827dd6772368ca7
SangeetaBobati <sangeetabobati@gmail.com>
2022-02-15 17:14:04 +0530

    Add project files.

---
+++ Model/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fullstack.Model
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Model/EFDBhandle.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Fullstack.Model
+{
+    public class EFDBhandle:DbContext
+    {
+
+        public string conn = "Server=DESKTOP-2VNS0L4\\SQLEXPRESS;Database=Customer_Tbl;Trusted_Connection=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsB)
+        {
+            optionsB.UseSqlServer(conn);
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+       
+    }
+}
+
